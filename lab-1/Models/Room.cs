a319f6983d1293f6b1da4e5bc97b0fa603730e5a
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-03-27 22:48:07 -0500

    Create Room Model

---
+++ lab-1/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAnnotations.Models
+{
+    public class Room
+    {
+        [Key]
+        public string RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public Section Section { get; set; }
+    }
+    public enum Section
+    {
+        First,
+        Second,
+        Third
+    }
+}

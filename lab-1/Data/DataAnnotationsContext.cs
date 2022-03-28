c95fc901366c221b8f85499cbbcbe1469e0d9de4
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-03-27 23:05:54 -0500

    Link SQLEXPRESS using Microsoft.EntityFrameworkCore

---
+++ lab-1/Data/DataAnnotationsContext.cs
@@ -0,0 +1,16 @@
+﻿using DataAnnotations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAnnotations.Data
+{
+    public class DataAnnotationsContext : DbContext
+    {
+        public DataAnnotationsContext(DbContextOptions<DataAnnotationsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using DataAnnotations.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataAnnotationsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataAnnotationsContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

84e5cbcd36877abdfa64b91a45f1e66f8c791bd4
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-03-28 00:10:53 -0500

    Change Client First And Last Name To Varchar

--- lab-1/Models/Client.cs
+++ lab-1/Models/Client.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataAnnotations.Models
 {
@@ -7,10 +8,12 @@ namespace DataAnnotations.Models
         public int Id { get; set; }
 
         [Display(Name = "First Name")]
+        [Column(TypeName = "varchar(150)")]
         [StringLength(25, MinimumLength = 3)]
         public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
+        [Column(TypeName = "varchar(150)")]
         [Required(ErrorMessage = "Last Name Required")]
         [StringLength(25, MinimumLength = 3)]
         public string LastName { get; set; }
